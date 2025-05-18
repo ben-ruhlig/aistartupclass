9989e1444203d059c76d24218976db1be3c05a70
Benjamin Ruhlig <benjamin.ruhlig@gmail.com>
2025-05-18 12:06:43 +0800

    basic init setup

---
+++ ai_app/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse(
+        "<h1>Hello from Django!</h1><p>Your AI application is running with Django, and served by nginx on your droplet!</p>"
+    )
